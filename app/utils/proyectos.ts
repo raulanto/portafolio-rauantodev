e766371d9a70381c5d40c2bc88e0848e0bb69a7a
rauanto <74162376+raulanto@users.noreply.github.com>
2025-10-02 23:31:14 -0600

    cambios

--- app/utils/proyectos.ts
+++ app/utils/proyectos.ts
@@ -1,57 +1,5 @@
 const list = [
-    {
-        id: 1,
-        title: 'Angular',
-        description: 'Practicas realizadas en Angular',
-        tag: [
-            {
-                id: '1',
-                name: 'Angular',
-                color: 'error'
-            },
-        ],
-        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYrjkPeRkL_Q3RThOD5F8Kow8fEKHJMcnwuA&s',
-        link: 'https://portafolio-angular-y7o3.vercel.app/home'
-    },
-    {
-        id: 2,
-        title: 'Sistema de Factura',
-        description: 'Practicas realizadas en Angular',
-        tag: [
-            {
-                id: '1',
-                name: 'Python',
-                color: 'primary'
-            },
-            {
-                id: '2',
-                name: 'flet',
-                color: 'error'
-            },
-        ],
-        imageUrl: './grad3.png',
-        link: 'https://github.com/raulanto/proyectoFacturaas'
-    },
 
-    {
-        id: 6,
-        title: 'api Chamge',
-        description: 'api para guardar habitos y rutinas',
-        tag: [
-            {
-                id: 1,
-                name: 'Python',
-                color: 'primary'
-            },
-            {
-                id: 2,
-                name: 'django',
-                color: 'success'
-            },
-        ],
-        imageUrl: './grad.png',
-        link: 'https://github.com/raulanto/apiChangue'
-    },
     {
         id: 7,
         title: 'Calculadora',
@@ -227,6 +175,59 @@ const list = [
         ],
         imageUrl: './grad.png',
         link: 'https://github.com/raulanto/rpa'
+    },
+    {
+        id: 1,
+        title: 'Angular',
+        description: 'Practicas realizadas en Angular',
+        tag: [
+            {
+                id: '1',
+                name: 'Angular',
+                color: 'error'
+            },
+        ],
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYrjkPeRkL_Q3RThOD5F8Kow8fEKHJMcnwuA&s',
+        link: 'https://portafolio-angular-y7o3.vercel.app/home'
+    },
+    {
+        id: 2,
+        title: 'Sistema de Factura',
+        description: 'Practicas realizadas en Angular',
+        tag: [
+            {
+                id: '1',
+                name: 'Python',
+                color: 'primary'
+            },
+            {
+                id: '2',
+                name: 'flet',
+                color: 'error'
+            },
+        ],
+        imageUrl: './grad3.png',
+        link: 'https://github.com/raulanto/proyectoFacturaas'
+    },
+
+    {
+        id: 6,
+        title: 'api Chamge',
+        description: 'api para guardar habitos y rutinas',
+        tag: [
+            {
+                id: 1,
+                name: 'Python',
+                color: 'primary'
+            },
+            {
+                id: 2,
+                name: 'django',
+                color: 'success'
+            },
+        ],
+        imageUrl: './grad.png',
+        link: 'https://github.com/raulanto/apiChangue'
     },
 ]
 
